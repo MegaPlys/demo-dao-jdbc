021a60add41c81e833be8d77bcc0c770a4dfc654
MegaPlys <megaplaysoficial1@gmail.com>
2025-08-25 12:26:08 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,7 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@teste.com", new Date(), 3000.00, obj);
+		
 		System.out.println(seller);
 
 	}
